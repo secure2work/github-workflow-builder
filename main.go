c5b439ce000845ed2da63906fae6d8c1e9d39ca4
bruteforce1414 <bruteforce1414@gmail.com>
2021-06-09 16:55:49 +0500

    feat: templates

--- main.go
+++ main.go
@@ -3,27 +3,69 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
-
+	"io"
+	"log"
+	"os"
+	"text/template"
 )
 
-func main() {
+func main()  {
 
 	var token, repo string
-	flag.StringVar(&token, "token", "", "token for github")
+	flag.StringVar(&token, "token.txt", "", "token.txt for github")
 	flag.StringVar(&repo, "repo", "", "name of github repository")
 	flag.Parse()
 
-	fmt.Println("token is", token)
-	fmt.Println("repo is", repo)
-
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token}, // repos - gmlewis account
+		&oauth2.Token{AccessToken: token},
 	)
-	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	tc := oauth2.NewClient(context.Background(), ts)
 	client := github.NewClient(tc)
+
+	fileTemplate, err := os.Open("template_action.yml")
+	if err != nil {
+		os.Exit(1)
+	}
+	defer func() {
+		if err = fileTemplate.Close(); err != nil {
+			os.Exit(1)
+		}
+	}()
+
+	b, err := io.ReadAll(fileTemplate)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	t:= template.Must(template.New("template_action").Delims("??", "??").Parse(string(b)))
+
+	fileAction, err := os.Create("action.yml")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	defer func() {
+		if err = fileTemplate.Close(); err != nil {
+			os.Exit(1)
+		}
+	}()
+
+
+	pluginName:= struct {
+		PluginName string
+	}{repo}
+	err = t.Execute(fileAction, pluginName)
+	if err != nil {
+		log.Println(err)
+	}
+
+	b, err = io.ReadAll(fileAction)
+	if err != nil {
+		os.Exit(1)
+	}
+
 
 	commitOption:= &github.RepositoryContentFileOptions{
 		Branch:  github.String("main"),
@@ -36,10 +78,10 @@ func main() {
 			Name:  github.String("bruteforce1414"),
 			Email: github.String("bruteforce1414@gmail.com"),
 		},
-		Content: []byte("this is my content"),
+		Content: b,
 	}
 
 
-	client.Repositories.CreateFile(context.Background(), "secure2work", "github-workflow-builder", "test.txt", commitOption)
+	client.Repositories.CreateFile(context.Background(), "secure2work", "github-workflow-builder", ".github/workflows/action.yml", commitOption)
 
 }
